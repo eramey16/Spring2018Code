fd64e9b43a30e62696a874d3ba18b77bd18b99d8
Emily Ramey <emily.ramey@wustl.edu>
2018-01-19 16:54:12 -0500

    first commit

---
+++ testFFT.cpp
@@ -0,0 +1,124 @@
+#include <string>
+#include <iostream>
+#include <fftw3.h>
+#include <random>
+#include <complex>
+#include <cstring>
+#include <string.h>
+#include <memory.h>
+
+using namespace std;
+
+int N = 1024;
+
+// Prints a distribution from a histogram array
+// scale changes number of stars per bin
+// binSize changes range of each bin 
+void printDistribution(int hist[], int binSize = 1, int scale = 1){
+	for(int i=0;i<255;i+=binSize){
+        int stars = 0;
+		for(int j=i;j<i+binSize;++j){
+			stars+= hist[j];
+		}
+        for(int j=0;j<stars/scale;++j){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+// Performs a 1D complex-to-complex FFT
+void FFT(fftw_complex in[], fftw_complex out[]){
+	fftw_plan p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+	fftw_execute(p);
+	fftw_destroy_plan(p);
+}
+
+// Performs a 1D complex-to-complex IFFT
+void IFFT(fftw_complex in[], fftw_complex out[]){
+	fftw_plan p = fftw_plan_dft_1d(N, in, out, FFTW_BACKWARD, FFTW_ESTIMATE);
+	fftw_execute(p);
+	fftw_destroy_plan(p);
+}
+
+int main()
+{
+	// distribution generator
+    mt19937 gen;
+	// distribution
+    normal_distribution<double> distr(0, 20);
+	
+	// setting up arrays to hold initial data and histograms
+    char dataIn[N][4] = {0};
+	int hist[4][255] = {0};
+	
+	// loop through data array
+	for(int i=0;i<4;++i){
+		for(int j=0;j<N;++j){
+			char num = (char)(distr(gen)); // randomly generate character
+			dataIn[j][i] = num; // record in data
+			hist[i][num+128]++; // add to histogram
+		}
+	}
+	
+	// Histogram titles
+	string words[4] = {"X Real", "X Imaginary", "Y Real", "Y Imaginary"};
+	cout << endl;
+	
+	// Print all histograms
+	for(int i=0;i<4;++i){
+		cout << words[i] << endl << endl;
+		printDistribution(hist[i], 16, 8);
+	}
+	
+	// complex arrays to hold initial data
+	fftw_complex xVals[N];
+	fftw_complex yVals[N];
+	
+	// get initial data in fftw_complex format
+	for(int i=0;i<N;++i){
+		complex<double> x(dataIn[i][0], dataIn[i][1]);
+		complex<double> y(dataIn[i][2], dataIn[i][3]);
+		memcpy(&xVals[i], &x, sizeof(fftw_complex));
+		memcpy(&yVals[i], &y, sizeof(fftw_complex));
+	}
+	
+	// complex arrays to hold fourier transformed data
+	fftw_complex xFFT[N];
+	fftw_complex yFFT[N];
+	
+	// complex arrays to hold IFFT output data
+	fftw_complex xOut[N];
+    fftw_complex yOut[N];
+	
+	// Perform FFT/IFFT on initial data
+	FFT(xVals, xFFT);
+	FFT(yVals, yFFT);
+	IFFT(xFFT, xOut);
+	IFFT(yFFT, yOut);
+	
+	// array to hold char format of output data
+	char dataOut[N][4] = {0};
+	
+	// loop through output data
+	for(int i=0;i<N;++i){
+		// put values in a temporary array for loop
+		double d[] = {xOut[i][0], xOut[i][1], yOut[i][0], yOut[i][1]};
+		// loop through values
+		for(int j=0;j<4;++j){
+			// round the value and scale down by N to offset scaling from IFFT
+			if(d[j] > 0){
+				dataOut[i][j] = (char)(d[j]/N+0.5);
+			}
+			else{
+				dataOut[i][j] = (char)(d[j]/N-0.5);
+			}
+			// check value against initial value
+			if(dataIn[i][j] != dataOut[i][j]){
+				// will print if data does not match
+				cout << "Values do not match: " << (int)dataIn[i][j] << ", " << (int)dataOut[i][j] << endl;
+			}
+		}
+	}
+}
+
